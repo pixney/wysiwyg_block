558179cdb3e7e05d30a60271a2aa2406abc536cf
William <william@pixney.com>
2018-12-07 23:37:02 +0100

    Cleanup

--- src/WysiwygBlockExtension.php
+++ src/WysiwygBlockExtension.php
@@ -8,24 +8,16 @@ use Pixney\WysiwygBlockExtension\Block\BlockModel;
 class WysiwygBlockExtension extends BlockExtension
 {
     protected $provides = 'anomaly.module.blocks::block.wysiwyg';
-    //protected $view     = 'theme::blocks/wysiwyg';
-    protected $model = BlockModel::class;
+    protected $model    = BlockModel::class;
 
     public function getView()
     {
-        // dd('pixney.theme.esharawater::blocks/wysiwyg');
-        // TODO:: GET active theme and do shit
-
+        // TODO : Either get active theme or config..
         return 'pixney.theme.esharawater::blocks/wysiwyg';
-    }
 
-    // if ('theme::blocks/wysi') {
-        //     dd('ja');
-        // }
         // if ($this->block->configuration('something_special')) {
         //     return 'my_company.extension.awesome_block::alternate_view';
         // }
-
         // return $this->view;
-    //}
+    }
 }
